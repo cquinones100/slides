714f81ca2d66d594c0c7ac5fdbfc52b2d2d059bb
Carlos Quinones <cquinones100@gmail.com>
2019-11-10 21:31:24 -0500

    adds formatting to message

--- lib/slides/formatters/message.rb
+++ lib/slides/formatters/message.rb
@@ -1,25 +1,65 @@
 module Slides
   module Formatters
     class Message < Formatter
-      BOLD_PREFIX = '\e[1m'.freeze
+      FORMAT_PREFIX = "\e[".freeze
+      FORMAT_POSTFIX = 'm'.freeze
+      FORMATS = { 'bold' => '1', 'underline' => '4' }.freeze
 
       private
 
       def formatted
-        apply_bold(raw)
+        apply_formatting(raw)
       end
 
-      def apply_bold(string)
-        string.gsub(/'\*{2}(\w*\s?)*\*{2}'/) do |match|
-          words = match.gsub(/'\*{2}|\*{2}'/, '').split(' ')
+      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      def apply_formatting(string)
+        string.gsub(formats_text_regex) do |match|
+          matched_formats = match.match(formats_start_regex)[0].split(',')
 
-          words.each_with_object('')
-            .with_index do |(word, return_string), index|
-            return_string << BOLD_PREFIX
-            return_string << word
-            return_string << ' ' unless index == words.size - 1
+          format_values = matched_formats
+            .each_with_object('')
+            .with_index do |(format, format_value_strings), index|
+
+            format_value_strings << FORMATS[format]
+            format_value_strings << ';' unless index == matched_formats.size - 1
           end
+
+          match_text = match.gsub(formats_regex, '').gsub(formats_regex, '')
+          matched_words = match_text.split(' ')
+
+          matched_words.each_with_index do |word, index|
+            word_regex = if index == 0
+                           /(?<=\s)?#{word}(?=\s)/
+                         elsif index == matched_words.size - 1
+                           /(?<=\s)#{word}(?=\s)?/
+                         else
+                           /(?<=\s)#{word}(?=\s)/
+                         end
+
+            match_text.gsub!(word_regex) do |matched_word|
+              FORMAT_PREFIX + format_values + FORMAT_POSTFIX + matched_word
+            end
+          end
+
+          match_text.strip + "\e[0m"
         end
+      end
+      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+
+      def formats_regex
+        /'(((#{formats.join('|')}),*)*)'/
+      end
+
+      def formats_start_regex
+        /(?<=\A')(((#{formats.join('|')}),*)*)/
+      end
+
+      def formats_text_regex
+        /#{formats_regex}[\w\W]*#{formats_regex}/
+      end
+
+      def formats
+        FORMATS.keys
       end
     end
   end
--- spec/slides/formatters/message_spec.rb
+++ spec/slides/formatters/message_spec.rb
@@ -4,13 +4,21 @@ RSpec.describe Slides::Formatters::Message do
       context 'when all text is bolded' do
         it 'prints the text bolded' do
           block = proc do
-            '**hi this is something bolded**'
+            'bold'
+            hi this is something bolded
+
+            things
+            'bold'
           end
 
           formatted_text = described_class.new(&block)
 
           expect(formatted_text)
-            .to eq('\e[1mhi \e[1mthis \e[1mis \e[1msomething \e[1mbolded')
+            .to eq(
+              "\e[1mhi \e[1mthis \e[1mis \e[1msomething \e[1mbolded" \
+              "\n\n" \
+              "\e[1mthings\e[0m"
+            )
         end
       end
 
@@ -19,15 +27,79 @@ RSpec.describe Slides::Formatters::Message do
           block = proc do
             hi this is text
 
-            '**hi this is something bolded**'
+            'bold'
+            hi this is something bolded
+            'bold'
           end
 
           formatted_text = described_class.new(&block)
 
           expect(formatted_text)
             .to eq(
-              "hi this is text\n\n\\e[1mhi \\e[1mthis \\e[1mis" \
-              ' \\e[1msomething \\e[1mbolded'
+              "hi this is text\n\n\e[1mhi \e[1mthis \e[1mis" \
+              " \e[1msomething \e[1mbolded\e[0m"
+            )
+        end
+      end
+    end
+
+    context 'when text is underlined' do
+      context 'when all the text is underlined' do
+        it 'prints the text underlined' do
+          block = proc do
+            'underline'
+            hi this is something underlined
+            'underline'
+          end
+
+          formatted_text = described_class.new(&block)
+
+          expect(formatted_text)
+            .to eq(
+              "\e[4mhi \e[4mthis \e[4mis \e[4msomething \e[4munderlined\e[0m"
+            )
+        end
+      end
+
+      context 'when some text is underlined' do
+        it 'prints the text underlined' do
+          block = proc do
+            'underline'
+            hi this is something underlined
+            'underline'
+
+            hi this is text
+          end
+
+          formatted_text = described_class.new(&block)
+
+          expect(formatted_text)
+            .to eq(
+              "\e[4mhi \e[4mthis \e[4mis \e[4msomething \e[4munderlined\e[0m" \
+              "\n\n" \
+              'hi this is text'
+            )
+        end
+      end
+
+      context 'when adding to an existing formatting' do
+        it 'adds underlining to the text' do
+          block = proc do
+            'underline,bold'
+                hi this is something underlined
+            'underline,bold'
+
+            hi this is text
+          end
+
+          formatted_text = described_class.new(&block)
+
+          expect(formatted_text)
+            .to eq(
+              "\e[4;1mhi \e[4;1mthis \e[4;1mis \e[4;1msomething " \
+              "\e[4;1munderlined\e[0m" \
+              "\n\n" \
+              'hi this is text'
             )
         end
       end
