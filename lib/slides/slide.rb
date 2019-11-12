6eb61bdf70f230c06bad3f9074e178ca64221b85
Carlos Quinones <cquinones100@gmail.com>
2019-11-11 23:28:44 -0500

    removes unnecessary method

--- lib/slides/slide.rb
+++ lib/slides/slide.rb
@@ -121,21 +121,5 @@ module Slides
     def window_width
       base.window_width
     end
-
-    def block_source(block)
-      require 'pry'
-
-      file, line = block.source_location
-
-      source_text = Pry::Code.from_file(file).expression_at(line)
-
-      string = source_text.scan(/do([.\S\s]*)end/).flatten.first
-
-      leading_white_space = string.scan(/\A\s*/).first.delete("\n")
-
-      string.strip.split("\n").map do |string_line|
-        string_line.sub(leading_white_space, '')
-      end.join("\n")
-    end
   end
 end
